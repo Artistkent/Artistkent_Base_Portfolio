d343429e35c0603c25bb7ac6901d8a8c7ea8ac3c
Artistkent <collins.ose5@gmail.com>
2023-08-14 13:30:30 +0100

    changes

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-import './style.css'
+
 
 import * as THREE from 'three';
 
 
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene;
 
